11a4e0dac89c6a91181f2cab08e70baceee6dcb1
brunodias77 <brunohdias95@gmail.com>
2026-05-26 15:23:15 -0300

    ...

---
+++ modules/user/user-application/src/main/java/com/btree/user/application/usecase/auth/verify_email/VerifyEmailUseCase.java
@@ -0,0 +1,11 @@
+package com.btree.user.application.usecase.auth.verify_email;
+
+import com.btree.shared.usecase.UseCase;
+
+public class VerifyEmailUseCase implements UseCase<VerifyEmailInput, Void> {
+
+    @Override
+    public Void execute(VerifyEmailInput input) {
+    }
+
+}
