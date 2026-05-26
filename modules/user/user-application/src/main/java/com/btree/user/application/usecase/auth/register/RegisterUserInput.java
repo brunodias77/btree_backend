74aee7f733c294e35945ac209adb8b68e6bc2aa2
brunodias77 <brunohdias95@gmail.com>
2026-05-26 09:52:52 -0300

    ...

---
+++ modules/user/user-application/src/main/java/com/btree/user/application/usecase/auth/register/RegisterUserInput.java
@@ -0,0 +1,4 @@
+package com.btree.user.application.usecase.auth.register;
+
+public record RegisterUserInput() {
+}
---
+++ modules/user/user-application/src/main/java/com/btree/user/application/usecase/auth/register/RegisterUserUseCase.java
@@ -0,0 +1,4 @@
+package com.btree.user.application.usecase.auth.register;
+
+public class RegisterUserUseCase {
+}
