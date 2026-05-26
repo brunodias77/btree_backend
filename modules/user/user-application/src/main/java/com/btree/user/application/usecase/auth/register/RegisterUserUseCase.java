07e0548f78bef5b13697d9b65ae57e26915cb0e5
brunodias77 <brunohdias95@gmail.com>
2026-05-26 09:53:13 -0300

    ...

--- modules/user/user-application/src/main/java/com/btree/user/application/usecase/auth/register/RegisterUserUseCase.java
+++ modules/user/user-application/src/main/java/com/btree/user/application/usecase/auth/register/RegisterUserUseCase.java
@@ -1,4 +1,12 @@
 package com.btree.user.application.usecase.auth.register;
 
-public class RegisterUserUseCase {
+import com.btree.shared.usecase.UseCase;
+import com.btree.shared.validation.Notification;
+import io.vavr.control.Either;
+
+public class RegisterUserUseCase implements UseCase<RegisterUserInput, RegisterUserOutput> {
+    @Override
+    public Either<Notification, RegisterUserOutput> execute(RegisterUserInput registerUserInput) {
+        return null;
+    }
 }
